129e618e0b0f7a3334322998e2f1ef352f660c48
SiQi Pan <SiQiPan2008@Outlook.com>
2025-04-07 21:47:42 +0800

    Init repo

---
+++ MD/WT/set_dihedral.py
@@ -0,0 +1,61 @@
+from math import *
+
+def rotate(point, axis, ref_point, angle):
+    rel_point = [point[i] - ref_point[i] for i in range(3)]
+    rel_new_point = [0, 0, 0]
+    angle_rad = angle *pi / 180
+    axis_normalized = [axis[i] / sqrt(sum([axis[j]**2 for j in range(3)])) for i in range(3)]
+    dot = sum([rel_point[i] * axis_normalized[i] for i in range(3)])
+    cross = [rel_point[(i+1)%3]*axis_normalized[(i+2)%3] - rel_point[(i+2)%3]*axis_normalized[(i+1)%3] for i in range(3)]
+    parallel = [axis_normalized[i] * dot for i in range(3)]
+    perpendicular = [rel_point[i] - parallel[i] for i in range(3)]
+    rotated_perpendicular = [perpendicular[i] * cos(angle_rad) + cross[i] * sin(angle_rad) for i in range(3)]
+    rel_new_point = [parallel[i] + rotated_perpendicular[i] for i in range(3)]
+    new_point = [rel_new_point[i] + ref_point[i] for i in range(3)]
+    return new_point
+
+def measure_dihedral(a, b, c, d):
+    ab = [a[i] - b[i] for i in range(3)]
+    bc = [b[i] - c[i] for i in range(3)]
+    cd = [c[i] - d[i] for i in range(3)]
+    ab_cross_bc = [ab[1]*bc[2] - ab[2]*bc[1], ab[2]*bc[0] - ab[0]*bc[2], ab[0]*bc[1] - ab[1]*bc[0]]
+    bc_cross_cd = [bc[1]*cd[2] - bc[2]*cd[1], bc[2]*cd[0] - bc[0]*cd[2], bc[0]*cd[1] - bc[1]*cd[0]]
+    ab_cross_bc_length = sqrt(sum([x**2 for x in ab_cross_bc]))
+    bc_cross_cd_length = sqrt(sum([x**2 for x in bc_cross_cd]))
+    cos_angle = sum([ab_cross_bc[i]*bc_cross_cd[i] for i in range(3)]) / (ab_cross_bc_length * bc_cross_cd_length)
+    dihedral = acos(cos_angle) * 180 / pi
+    return dihedral
+
+with open("coordinates_to_rotate.txt", "r") as file:
+    lines = file.readlines()
+
+# axis is b-c
+# pivot is c
+# dihedral is a-b-c-d
+# delete the first column of resi/resn in .gro file
+a = [float(lines[0].strip().split()[i]) for i in [2, 3, 4]]
+b = [float(lines[1].strip().split()[i]) for i in [2, 3, 4]]
+c = [float(lines[2].strip().split()[i]) for i in [2, 3, 4]]
+d = [float(lines[3].strip().split()[i]) for i in [2, 3, 4]]
+axis = [b[i] - c[i] for i in [0, 1, 2]]
+
+points_to_rotate = []
+for i in range (5, len(lines)):
+    points_to_rotate.append(lines[i].strip().split())
+
+target_dihedral = 170
+dihedral = measure_dihedral(a, b, c, d)
+#angle = 180
+angle = target_dihedral - dihedral
+
+points_rotated = []
+for point in points_to_rotate:
+    point_id = [point[0], point[1]]
+    rotated_pos = rotate([float(point[i]) for i in [2, 3, 4]], axis, c, angle)
+    rotated_pos = [f"{rotated_pos[i]:.3f}" for i in range(3)]
+    rotated_vel = rotate([float(point[i]) for i in [5, 6, 7]], axis, [0, 0, 0], angle)
+    rotated_vel = [f"{rotated_vel[i]:.4f}" for i in range(3)]
+    points_rotated.append(point_id + rotated_pos + rotated_vel)
+
+with open("coordinates_rotated.txt", "w") as file:
+    lines = file.writelines([" ".join(point) + "\n" for point in points_rotated])
---
+++ MD/plot_xvg_change_to_use.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def load_xvg(filename):
+    data = []
+    with open(filename, "r") as f:
+        for line in f:
+            if not line.startswith(("#", "@")):
+                data.append([float(x) for x in line.split()])
+    return np.array(data)
+
+def plot_running_avg(data):
+    window_size = 10
+    x = data[:, 0]
+    y = data[:, 1]
+    running_avg = np.convolve(y, np.ones(window_size) / window_size, mode="valid")
+    x_avg = x[len(x) - len(running_avg) : ]
+    plt.plot(x_avg, running_avg, label="10-ps Running Avg", color="red")
+
+filename = "QTY/rmsd_prot_trans"
+data = load_xvg(f"{filename}.xvg")
+plt.figure(figsize=(8, 5))
+plt.plot(data[:, 0], data[:, 1], label="", color="black")
+#plot_running_avg(data)
+plt.xlabel("time (ns)")
+plt.ylabel("RMSD (nm)")
+plt.title("")
+#plt.xlim(-180, 180)
+#plt.legend()
+plt.grid(False)
+plt.savefig(f"{filename}.pdf", format="pdf", bbox_inches="tight")
+plt.show()
