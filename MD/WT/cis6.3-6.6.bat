eb5f2f80035dd9d606be37d9763e35e23d4c1742
SiQi Pan <SiQiPan2008@Outlook.com>
2025-04-12 09:05:14 +0800

    finish cis run for WT

---
+++ MD/WT/cis6.3-6.6.bat
@@ -0,0 +1,10 @@
+gmx grompp -f step6.3_equilibration.mdp -o cis6.3.tpr -p topol_cis.top -n index.ndx -c cis6.2.gro -r cis6.2.gro -t cis6.2.cpt
+gmx mdrun -deffnm cis6.3 -v
+gmx grompp -f step6.4_equilibration.mdp -o cis6.4.tpr -p topol_cis.top -n index.ndx -c cis6.3.gro -r cis6.3.gro -t cis6.3.cpt
+gmx mdrun -deffnm cis6.4 -v
+gmx grompp -f step6.5_equilibration.mdp -o cis6.5.tpr -p topol_cis.top -n index.ndx -c cis6.4.gro -r cis6.4.gro -t cis6.4.cpt
+gmx mdrun -deffnm cis6.5 -v
+gmx grompp -f step6.6_equilibration.mdp -o cis6.6.tpr -p topol_cis.top -n index.ndx -c cis6.5.gro -r cis6.5.gro -t cis6.5.cpt
+gmx mdrun -deffnm cis6.6 -v
+gmx grompp -f step7_production.mdp      -o cis7.tpr   -p topol_cis.top -n index.ndx -c cis6.6.gro -r cis6.6.gro -t cis6.6.cpt
+gmx mdrun -deffnm cis7 -v
