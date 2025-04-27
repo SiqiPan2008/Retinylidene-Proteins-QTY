12a5284ab32f81bbc9bf45e1cddfa7e874096891
SiQi Pan <SiQiPan2008@Outlook.com>
2025-04-27 22:58:21 +0800

    Prepare MD simulation results demo

--- MD/plot_xvg_change_to_use.py
+++ MD/plot_xvg_change_to_use.py
@@ -10,21 +10,22 @@ def load_xvg(filename):
     return np.array(data)
 
 def plot_running_avg(data):
-    window_size = 10
+    window_size = 1000
     x = data[:, 0]
     y = data[:, 1]
     running_avg = np.convolve(y, np.ones(window_size) / window_size, mode="valid")
     x_avg = x[len(x) - len(running_avg) : ]
     plt.plot(x_avg, running_avg, label="10-ps Running Avg", color="red")
 
-filename = "QTY/rmsd_transprot"
+filename = "WT/rmsd_transprot"
 data = load_xvg(f"{filename}.xvg")
 plt.figure(figsize=(8, 5))
 plt.plot(data[:, 0], data[:, 1], label="", color="black")
-#plot_running_avg(data)
+plot_running_avg(data)
 plt.xlabel("time (ns)")
 plt.ylabel("RMSD (nm)")
 plt.title("")
+plt.ylim(0, 0.35)
 #plt.xlim(-180, 180)
 #plt.legend()
 plt.grid(False)
