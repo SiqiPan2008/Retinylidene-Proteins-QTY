08df6ece3d09f3a597d1d19fcf430b724eecb3e8
SIQI PAN <SiQiPan2008@outlook.com>
2025-05-10 10:35:40 +0800

    Clean up files except for MD/QTY and MD/WT.

--- MD/Analysis/cmd_ie_qty_cis2.bat
+++
@@ -1,7 +0,0 @@
-gmx grompp -f qty_cis_ie_pocket.mdp -c qty_cis.gro -t qty_cis.cpt -p qty_cis.top -n qty_cis.ndx -o qty_cis_ie_pocket.tpr
-gmx mdrun -rerun qty_cis.xtc -deffnm qty_cis_ie_pocket -nb cpu
-echo 21 22 0 | gmx energy -f qty_cis_ie_pocket.edr -o qty_cis_ie_pocket.xvg
-
-gmx grompp -f qty_cis_ie_prot.mdp -c qty_cis.gro -t qty_cis.cpt -p qty_cis.top -n qty_cis.ndx -o qty_cis_ie_prot.tpr
-gmx mdrun -rerun qty_cis.xtc -deffnm qty_cis_ie_prot -nb cpu
-echo 21 22 0 | gmx energy -f qty_cis_ie_prot.edr -o qty_cis_ie_prot.xvg
--- MD/WT-wrong-ff/steps6.5to7.bat
+++
@@ -1,6 +0,0 @@
-gmx grompp -f step6.5_equilibration.mdp -c step6.4.gro -r step6.4.gro -t step6.4.cpt -n index.ndx -o step6.5.tpr
-gmx mdrun -deffnm step6.5 -v
-gmx grompp -f step6.6_equilibration.mdp -c step6.5.gro -r step6.5.gro -t step6.5.cpt -n index.ndx -o step6.6.tpr
-gmx mdrun -deffnm step6.6 -v
-gmx grompp -f step7_production.mdp -c step6.6.gro -r step6.6.gro -t step6.6.cpt -n index.ndx -o step7.tpr
-gmx mdrun -deffnm step7 -v
